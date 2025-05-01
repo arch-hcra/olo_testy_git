0b190dbcc228ca0042360fdd0516dfb9f2560d55
archhcra <bomcom@bk.ru>
2025-05-01 18:27:07 +0300

    4_test

--- test_123.py
+++ test_123.py
@@ -14,6 +14,7 @@ def test_equal_first_characters():
 def test_different_types():
     assert compare_numbers("abc", "123") == "No"
 
+
 def test_empty_first_input():
     with pytest.raises(IndexError):
         compare_numbers("", "123")
@@ -21,3 +22,14 @@ def test_empty_first_input():
 def test_empty_second_input():
     with pytest.raises(IndexError):
         compare_numbers("123", "")
+
+def test_integer_input_equal():
+    assert compare_numbers(123, 123) == "Yes"
+
+def test_integer_input_different():
+    assert compare_numbers(123, 456) == "No"
+
+def test_integer_string_mixed():
+    assert compare_numbers(123, "123") == "Yes"
+    assert compare_numbers("123", 123) == "Yes"
+    assert compare_numbers(123, "456") == "No"
